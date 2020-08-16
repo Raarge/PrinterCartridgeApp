e04105186797cf2338a76297ac77e46a6289b708
Darin Campbell - Raarge Actual <animusdrogai@gmail.com>
2020-08-16 00:56:58 -0400

    Fixed an issue with the last row of the printer view table not being displayed.

--- PrinterCartridgeApp/Controllers/PrinterController.cs
+++ PrinterCartridgeApp/Controllers/PrinterController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 using PrinterCartridgeApp.Data;
 using PrinterCartridgeApp.Interfaces;
 using PrinterCartridgeApp.Models;
